60bea597960e68d5e7215dcd2abf5ada1e34d10d
Malek <stonemokkaks@gmail.com>
2024-08-25 17:41:17 +0300

    fixed issues

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -5,9 +5,7 @@ import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
-import CardHeader from '@mui/material/CardHeader'
-import Avatar from '@mui/material/Avatar'
-import IconButton from '@mui/material/IconButton'
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
